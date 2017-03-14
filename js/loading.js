02734d241bec2020cad08aa34b5033bcb4e6dd14
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-03-14 21:32:40 +0900

    2017.03.14

---
+++ js/loading.js
@@ -0,0 +1,22 @@
+$( document ).ready( function() {
+  var index = Math.floor((Math.random() * 17)); //17개 중에서 할때마다 랜덤하게 값 나오도록
+  var category = 'animals',
+      effects = ['tada', 'pulse', 'flipInX', 'flipInY', 'fadeIn', 'fadeInUp', 'fadeInRight', 'fadeInUpBig',
+          'fadeInRightBig', 'slideInRight', 'bounceIn', 'bounceInUp', 'bounceInRight', 'rotateIn',
+          'rotateInUpLeft', 'rotateInUpRight', 'lightSpeedIn'];
+
+
+
+  $(window).load(function() {
+    $('.js--lazyload').hide(),
+
+    $.lazyLoadXT.onload = function() {
+        var $el = $("#main"); //이미지 태그 아이디로 이미지 태그 가져와서
+        $el
+            .removeClass('lazy-hidden')
+            .addClass('animated ' + effects[index]);
+    };
+
+  });
+
+});
--- js/upcomingshows.js
+++ js/upcomingshows.js
@@ -2,28 +2,47 @@
 $(document).ready(function(){
 
     $(".show_title").first("dt").css('color','#fcbe46');
+    // $(".show_title>.show_desc:not(:first)").find(".show_desc").css("display","none");
 
-    // show_wrap 클래스 바로 하위에 있는 show_title를 클릭했을 때
+    //show_wrap 클래스 바로 하위에 있는 show_title를 클릭했을 때
+    // $(".show_title").click(function(){
+    //     $(".show_title").first("dt").css('color','#FFF');
+    //
+    //     var show_desc = $(this).next(".show_desc");
+    //
+    //     // show_desc 가 화면상에 보일 때 위로 접기
+    //     if( show_desc.is(":visible") )
+    //     {
+    //         $(".show_desc").slideUp();
+    //         $(".show_title").css('color', '#FFF');
+    //     }
+    //     else{ // 아니면 아래로 펼치기
+    //         show_desc.slideDown();
+    //         $(this).css('color', '#fcbe46');
+    //     }
+    // });
+
+
+
+    //accordian menu
     $(".show_title").click(function(){
-        $(".show_title").first("dt").css('color','#FFF');
+      $(".show_title").first("dt").css('color','#FFF');
+  		$(".show_desc").slideUp();
+      $(".show_title").css('color', '#FFF');
 
-        var show_desc = $(this).next(".show_desc");
-
-        // show_desc 가 화면상에 보일 때 위로 접기
-        if( show_desc.is(":visible") )
-        {
-            $(".show_desc").slideUp();
-            $(".show_title").css('color', '#FFF');
-        }
-        else{ // 아니면 아래로 펼치기
-            show_desc.slideDown();
-            $(this).css('color', '#fcbe46');
-        }
+      if(!$(this).next().is(":visible"))
+  		{
+  			$(this).next().slideDown();
+        $(this).css('color', '#fcbe46');
+  		}
     });
 
+    // $("#accordian h3").click(function(){
+    // 		$("#accordian ul ul").slideUp();
+    // 		if(!$(this).next().is(":visible"))
+    // 		{
+    // 			$(this).next().slideDown();
+    // 		}
+    // 	})
 
-    // $( ".show_title" ).click(function() {
-    //    $(this).css('color', '#fcbe46');
-    //   $( ".show_desc" ).slideToggle( "fast" );
-    // });
 });
