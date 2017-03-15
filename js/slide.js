3d67ce6bab372375c03092ea9b918889cb28e959
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-03-15 18:39:17 +0900

    2017.03.15

---
+++ js/slide.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function ($) {
+
+	var time = 3000;//milliseconds
+	var index = 0;
+	var container = $("#twitter_txt");
+	var childrenCount = $(".section").length;
+	function slideToNext() {
+
+	    index = (index + 1) % childrenCount;
+	    console.log(index);
+	    container.css({
+	        marginLeft: -1 * index * 100 + "%"
+	    })
+	}
+	var pt = window.setInterval(function() {
+	    slideToNext();
+	}, time)
+
+});
