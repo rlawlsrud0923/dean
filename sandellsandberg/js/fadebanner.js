0191da0059945d81103be6049d8e1f4dc1b6dbe3
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-06-14 10:47:32 +0000

    2017 / 06 / 14

--- sandellsandberg/js/fadebanner.js
+++ sandellsandberg/js/fadebanner.js
@@ -9,12 +9,16 @@ $(document).ready(function(){
 
   $('.fadebanner').on({
     mouseenter:function(){
+      //console.log("mouseenter");
+      
       clearInterval(fadeheand)
-      $('.fadebanner button, .fadebanner li a').css('display','block')//마우스enter일 경우 버튼 보이게
+      $('.fadebanner button, .fadebanner li a').css('display','block');//마우스enter일 경우 버튼 보이게
     },
     mouseleave:function(){
+      //console.log("mouseleave");
+      
       fadeheand = setInterval(fademotion,3800)
-      $('.fadebanner button,.fadebanner li a').css('display','none')//마우스enter일 경우 버튼 숨기기
+      $('.fadebanner button,.fadebanner li a').css('display','none');//마우스leave일 경우 버튼 숨기기
     }
   });
 
@@ -22,10 +26,12 @@ $(document).ready(function(){
 
 
   function fademotion(){
+    console.log("fademotion");
+    
     $('.fadebanner ul li img').eq(fadeIdx).removeClass('on').fadeOut()
       fadeIdx ++
     
-    $('.fadebanner ul li img').eq(fadeIdx).addClass('on').fadeIn()
+    // $('.fadebanner ul li img').eq(fadeIdx).addClass('on').fadeIn()
 
     if(fadeIdx == $('.fadebanner ul li').length){
       fadeIdx = 0
