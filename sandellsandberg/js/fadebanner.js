02970ae0c4b2ac8f91e881b689577b78a6a8b755
rlawlsrud0923 <rlawlsrud0923@gmail.com>
2017-06-14 09:41:43 +0000

    2017-06-14

--- sandellsandberg/js/fadebanner.js
+++ sandellsandberg/js/fadebanner.js
@@ -10,17 +10,18 @@ $(document).ready(function(){
   $('.fadebanner').on({
     mouseenter:function(){
       clearInterval(fadeheand)
-      $('.fadebanner button,.fadebanner li a').css('display','block')//마우스enter일 경우 버튼 보이게
+      $('.fadebanner button, .fadebanner li a').css('display','block')//마우스enter일 경우 버튼 보이게
     },
     mouseleave:function(){
       fadeheand = setInterval(fademotion,3800)
       $('.fadebanner button,.fadebanner li a').css('display','none')//마우스enter일 경우 버튼 숨기기
     }
-  })
+  });
 
   function fademotion(){
     $('.fadebanner ul li img').eq(fadeIdx).removeClass('on').fadeOut()
     fadeIdx ++
+    
     $('.fadebanner ul li img').eq(fadeIdx).addClass('on').fadeIn()
 
     if(fadeIdx == $('.fadebanner ul li').length){
